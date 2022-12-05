c33acb342e522066d946d8701d9b191c82512737
pdongre123 <49409303+pdongre123@users.noreply.github.com>
2022-12-05 17:02:16 +0530

    scrollbar design completed

---
+++ scrollbar_react/src/App.jsx
@@ -0,0 +1,46 @@
+import { useRef } from "react";
+import "./App.css";
+import Scrollbarreact from "./components/Scrollbarreact";
+
+function App() {
+  const services = useRef(null);
+  const blog = useRef(null);
+  const contact = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="App">
+      <Scrollbarreact />
+      <div className="hero">
+        <ul>
+          <li onClick={() => scrollToSection(services)} className="link">
+            Services
+          </li>
+          <li onClick={() => scrollToSection(blog)} className="link">
+            Blog
+          </li>
+          <li onClick={() => scrollToSection(contact)} className="link">
+            Contact
+          </li>
+        </ul>
+      </div>
+      <div ref={services} className="services">
+        <h3>Services</h3>
+      </div>
+      <div ref={blog} className="blog">
+        <h3>Blog</h3>
+      </div>
+      <div ref={contact} className="contact">
+        <h3>Contact</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
